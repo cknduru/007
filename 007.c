2f4f043670da85ebc66c668e366283225eb713d4
cnduru <johndoe@gmail.com>
2019-05-29 13:57:39 -0700

    Added empty netfilter (un)hooks
    Signed-off-by: cnduru <johndoe@gmail.com>

--- 007.c
+++ 007.c
@@ -1,17 +1,49 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+#include <linux/skbuff.h>
+#include <linux/udp.h>
+#include <linux/icmp.h>
+#include <linux/ip.h>
+#include <linux/inet.h>
+
+#define DIP "1.2.3.4"
+
+static struct nf_hook_ops nfho;     // net filter hook option struct 
+static struct net n;
+
+struct sk_buff *sock_buff;          // socket buffer used in linux kernel
+struct udphdr *udp_header;          // udp header struct (not used)
+struct iphdr *ip_header;            // ip header struct
+struct ethhdr *mac_header;          // mac header struct
+
 
 MODULE_DESCRIPTION("Monitor packets");
 MODULE_AUTHOR("cnduru");
 MODULE_LICENSE("GPL");
+
+unsigned int hook_func(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
+{
+	printk(KERN_INFO "hookl\n");
+    return 0;
+}
  
 int init_module()
 {
-        printk(KERN_INFO "module 007 is loaded into the kernel\n");
-        return 0;
+    nfho.hook = hook_func;
+    nfho.hooknum = 4; 
+    nfho.pf = PF_INET;
+    nfho.priority = NF_IP_PRI_FIRST; // give this our hook highest priority
+    
+    nf_register_net_hook(&n, &nfho);
+
+    printk(KERN_INFO "module 007 is loaded into the kernel\n");
+    return 0;
 }
  
 void cleanup_module()
 {
-        printk(KERN_INFO "module 007 has been unloaded\n");  
+    nf_unregister_net_hook(&n, &nfho);
+    printk(KERN_INFO "module 007 has been unloaded\n");  
 }
--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo rmmod dropicmp 2>/dev/null
+sudo rmmod 007 2>/dev/null
 make test
 if [ $? != 0 ]; then
             printf "Error when executing command: make"
